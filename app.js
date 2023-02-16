f00f016b2820cbc214d3ef7d0fa865d69ce9602d
PYOJAN <pintuprajapati4@gmail.com>
2023-02-16 23:14:28 +0530

    layout refactor, home-page, error-404-page completed

--- app.js
+++ app.js
@@ -11,12 +11,19 @@ app.use(express.static("public"));
 // Set template engine
 app.use(expressEjsLayouts);
 app.set("views", join(__dirname, "/resources", "/views"));
-app.set("layout", "./layouts/full-width");
+app.set("layout", "./layouts/main-layout");
 app.set("view engine", "ejs");
 
 // routes
 app.get("/", (req, res, next) => {
-  res.status(200).render("home", { title: "home page" });
+  res.status(200).render("pages/home", { title: "Real time Pizza App" });
 });
+
+app.all("*", (req, res, next) => {
+  res.status(404).render("pages/404/404", {
+    title: "ERROR",
+    layout: "./layouts/error-layout",
+  });
+})
 
 export default app;
